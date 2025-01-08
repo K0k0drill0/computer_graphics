039f297ca5fedc293420c760f10065e81bac7c3b
Anastasia Astakhova <asanastasia0201@gmail.com>
2025-01-08 17:56:47 +0300

    added demo

--- Engine/Engine/obj/Debug/net8.0-windows/Engine.AssemblyInfo.cs
+++ Engine/Engine/obj/Debug/net8.0-windows/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c929ccd7526689f397ed8f2c89ce066a2a2ef1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
